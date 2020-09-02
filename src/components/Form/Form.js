4dc0ec2bfd76625f65861a50c4635bae9f4076c6
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-09-02 10:30:49 -0600

    Render form elements

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -4,10 +4,33 @@ import './Form.css'
 class Form extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      songName: '',
+      artistName: '',
+      link: '',
+      id: Date.now()
+    }
   }
 
   render() {
-    
+    return (
+      <form className='form'>
+        <h2>Add a song here:</h2>
+        <input
+          name='songTitle'
+          placeholder='Song Title'
+        />
+        <input
+          name='artistName'
+          placeholder='ArtistName'
+        />
+        <input
+          name='link'
+          placeholder='https://www.youtube.com/watch?v=link'
+        />
+        <button>Add To Playlist</button>
+      </form>
+    )
   }
 }
 
