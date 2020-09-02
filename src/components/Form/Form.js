92a939bdeb0fff48d257cd50ec9044be8f4b3ba8
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-09-02 10:07:05 -0600

    Setup Form class component

---
+++ src/components/Form/Form.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Form.css'
+
+class Form extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    
+  }
+}
+
+export default Form;
